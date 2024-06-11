b571db992f29d8adcf628a1816a1715fde359d93
Hossein Mansouri <hossein.mansouri@medis.land>
2024-06-11 17:05:49 +0330

    create other crud actions

--- data_keeper_x/app/Http/Controllers/DataController.php
+++ data_keeper_x/app/Http/Controllers/DataController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\ResponseBodyBuilder;
 use App\Models\Data;
 use Exception;
+use Hamcrest\Type\IsNumeric;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -19,7 +21,7 @@ class DataController extends Controller
             "data" => "required",
         ]);
         if ($validation->fails()) {
-            return response($validation->messages(), 400);
+            return ResponseBodyBuilder::buildFailureResponse(null, $validation->messages());
         }
 
         //create records
@@ -41,12 +43,109 @@ class DataController extends Controller
 
             // save record
             if (!$dataRecord->save()) {
-                return response("save failed!");
+                return ResponseBodyBuilder::buildFailureResponse("save failed");
             }
         } catch (Exception $error) {
-            return response($error, 500);
+            return ResponseBodyBuilder::buildFailureResponse(null, $error, 500);
         }
 
-        return response(true);
+        return ResponseBodyBuilder::buildSuccessResponse();
+    }
+
+    public function getDataById($id)
+    {
+        // validating data
+        if (!is_numeric($id) || is_null($id) || empty($id)) {
+            return ResponseBodyBuilder::buildFailureResponse("id not valid!");
+        }
+
+        // get data from db
+        $dataRecord = Data::find($id);
+
+        // if record not exist
+        if (!$dataRecord) {
+            return ResponseBodyBuilder::buildFailureResponse("record id not found!");
+        }
+
+        // unserialize data
+        $dataRecord->data = unserialize($dataRecord->data);
+
+        return ResponseBodyBuilder::buildSuccessResponse(null, $dataRecord);
+    }
+
+    public function getAllData()
+    {
+        // get all records
+        $allRecords = Data::all();
+
+        // unserialize all records
+        foreach ($allRecords as $key => $value) {
+            $allRecords[$key]->data = unserialize($value->data);
+        }
+
+        return ResponseBodyBuilder::buildSuccessResponse(null, $allRecords);
+    }
+
+    public function Delete($id)
+    {
+        // validating data
+        if (!is_numeric($id) || is_null($id) || empty($id)) {
+            return ResponseBodyBuilder::buildFailureResponse("id not valid!");
+        }
+
+        // delete record
+        try {
+            $dataRecord = Data::find($id);
+
+            // save record
+            if (!$dataRecord->delete()) {
+                return ResponseBodyBuilder::buildFailureResponse("delete failed");
+            }
+        } catch (Exception $error) {
+            return ResponseBodyBuilder::buildFailureResponse(null, $error, 500);
+        }
+
+        return ResponseBodyBuilder::buildSuccessResponse();
+    }
+
+    public function Update(Request $request)
+    {
+        // validating data
+        $validation = Validator::make($request->all(), [
+            "data_id" => "numeric|required",
+            "relation_id" => "numeric",
+        ]);
+        if ($validation->fails()) {
+            return ResponseBodyBuilder::buildFailureResponse(null, $validation->messages());
+        }
+
+        // update record
+        try {
+            $record = Data::find($request->id);
+            $record->data = serialize($request->data);
+            // check data
+            if ($request->has("data")) {
+                $record->data = $request->data;
+            }
+            // check relation id
+            if ($request->has("relation_id")) {
+                $record->relation_id = $request->relation_id;
+            }
+            // check name
+            if ($request->has("name")) {
+                $record->name = $request->name;
+            }
+            // check signature
+            if ($request->has("signature")) {
+                $record->signature = $request->signature;
+            }
+
+            // save record
+            if (!$record->save()) {
+                return ResponseBodyBuilder::buildFailureResponse("save failed");
+            }
+        } catch (Exception $error) {
+            return ResponseBodyBuilder::buildFailureResponse(null, $error, 500);
+        }
     }
 }
--- data_keeper_x/routes/api.php
+++ data_keeper_x/routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataController;
 
 Route::post('create', [DataController::class, "Create"]);
+Route::patch('update', [DataController::class, "Update"]);
+Route::delete('/delete/{id}', [DataController::class, "Delete"]);
+Route::get('/data/{id}', [DataController::class, "getDataById"]);
+Route::get('list', [DataController::class, "getAllData"]);
