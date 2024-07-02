c9dfc048c47c67b7f67c458f362cb17c50b27d55
Hossein Mansouri <hsnmsri82@gmail.com>
2024-07-02 22:03:06 +0330

    create api secret token class

---
+++ data_keeper_x/app/Classes/ApiSecretToken/ApiSecretToken.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Classes\Token;
+
+use Firebase\JWT\JWT;
+
+class ApiSecretToken
+{
+    /**
+     * Generate a JWT token for a given user ID.
+     *
+     * This function generates a JSON Web Token (JWT) for the given user ID.
+     * The token is signed using the HS256 algorithm and includes the user ID
+     * as the subject (sub) and the issued at time (iat).
+     *
+     * @param int $user_id The ID of the user for whom the JWT is being generated.
+     * @return string The generated JWT token.
+     */
+    public static function make(int $user_id): string
+    {
+        return JWT::encode(
+            [
+                'sub' => $user_id,
+                'iat' => time(),
+            ],
+            env('JWT_SECRET'),
+            'HS256'
+        );
+    }
+}
