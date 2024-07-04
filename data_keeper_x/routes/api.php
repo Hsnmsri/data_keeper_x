cb4f00da0e5784f69da4da6d9dce85f8fb0e20f8
Hossein Mansouri <hsnmsri82@gmail.com>
2024-07-05 03:00:10 +0330

    create user routes

--- data_keeper_x/routes/api.php
+++ data_keeper_x/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AccessTokenCheck;
 
 // Application Routes
 Route::prefix("/")->group(function () {
@@ -11,8 +13,21 @@ Route::prefix("/")->group(function () {
     Route::get("verify_token", [AuthController::class, "verifyAccessToken"]);
 
     // reset password
-    Route::post("reset_password/request", [AuthController::class, "requestResetPassword"]);
-    Route::put('reset_password', [AuthController::class, 'resetPassword']);
+    Route::prefix("reset_password")->group(function () {
+        Route::post("request", [AuthController::class, "requestResetPassword"]);
+        Route::put('/', [AuthController::class, 'resetPassword']);
+    });
 
-    // role permissions
+    // user
+    Route::prefix("users")->group(function () {
+        Route::get("/", [UserController::class, "getUserList"]);
+        Route::post("/", [UserController::class, "createUser"]);
+        Route::get("{id}", [UserController::class, "getUserById"]);
+        Route::put("{id}", [UserController::class, "updateUser"]);
+        Route::put("{id}/email", [UserController::class, "updateUserEmail"]);
+        Route::put("{id}/password", [UserController::class, "updateUserPassword"]);
+        Route::put("{id}/role", [UserController::class, "updateUserRole"]);
+        Route::post("{id}/api_secret", [UserController::class, "renewUserApiSecret"]);
+        Route::delete("{id}", [UserController::class, "deleteUser"]);
+    })->middleware(AccessTokenCheck::class);
 });
